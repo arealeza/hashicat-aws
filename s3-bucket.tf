95a3b2cf608b5572bd5ed4054fa7744bf1da74f8
arealeza <132370663+arealeza@users.noreply.github.com>
2023-05-08 16:53:51 +0800

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,9 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source  = "app.terraform.io/arealeza/s3-bucket/aws"
+  version = "2.8.0"
 
   bucket_prefix = "andrea-realeza"
+  bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
