21cb25063fa74b8834c55a27953e7dd00b3af300
Andrea Realeza <realezama@ncs.com.sg>
2023-05-08 07:19:41 +0000

    update remote backend

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
@@ -8,14 +7,3 @@ terraform {
     }
   }
 }
-=======
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "arealeza"
-    workspaces {
-      name = "hashicat-aws"
-    }
-  }
-}
->>>>>>> fbc281eef926d1ac42ee8bbb58d1b6adee11dfe1
